e7d7931d658eb306aee6f51dc640ab091e5d0691
tiencoipro <tiencoipro@tiencoipros-MacBook-Pro.local>
2015-03-17 23:38:31 +0700

    first commit

---
+++ ColorPickerView/ColorPickerView.swift
@@ -0,0 +1,21 @@
+//
+//  ColorPickerView.swift
+//  ColorPickerView
+//
+//  Created by tiencoipro on 3/17/15.
+//  Copyright (c) 2015 tiencoipro. All rights reserved.
+//
+
+import UIKit
+
+class ColorPickerView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
